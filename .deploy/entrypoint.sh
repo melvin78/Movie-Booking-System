4d718f48ca2538d8bc9c37539d8a92e67e73cac1
melvin <melvin.mark@centrino.co.ke>
2022-09-01 12:18:23 +0300

    build & fix:re-write vendor files location

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -5,3 +5,4 @@ composer dump-autoload --no-interaction --no-dev --optimize
 echo "🎬 artisan commands"
 echo "🎬 start supervisord"
 supervisord -c /srv/app/.deploy/config/supervisor.conf
+caddy run --config /srv/app/.deploy/config/Caddyfile
